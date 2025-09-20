586a450306ea8bd7207fa901d4667c3613a2d20a
Aarav Desai <aaravd037@gmail.com>
2025-09-20 12:07:16 -0700

    Update main.py
    added the ability to change the wpm and increases errors as speed goes up

--- main.py
+++ main.py
@@ -2,15 +2,32 @@ import keyboard
 import random
 import time
 
-
 default_text = "The quick brown fox jumps over the lazy dog"
 text = input("What is the text you would like to be typed (typing d will type the default text)? \n")
+
 if text == "d":
     text = default_text
 else:
     pass
 
-chance_of_error = 1/10
+wpm = input("What is the WPM you would like this to be type at (or you can type s for a slow speed, m for a medium speed, f for a fast speed, and sf for a super fast speed)?\n")
+
+if wpm == "s":
+    wpm = 20
+if wpm == "m":
+    wpm = 50
+if wpm == "f":
+    wpm = 80
+if wpm == "sf":
+    wpm = 120
+else:
+    pass
+
+default_robot_speed = 19
+
+speed = wpm/default_robot_speed
+
+chance_of_error = (speed)/30
 letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 
 index = 0
@@ -18,7 +35,7 @@ debug_text = ""
 
 time.sleep(5)
 while index < len(text):
-    time.sleep(random.random())
+    time.sleep(random.random()/speed)
     if index == 0:
         keyboard.write(text[index])
         debug_text += text[index]
@@ -27,7 +44,7 @@ while index < len(text):
         letter = letters[random.randint(0,25)]
         keyboard.write(letter)
         debug_text += letter
-        time.sleep(random.random())
+        time.sleep(random.random()/speed)
         keyboard.send("delete")
         debug_text += "'del'"
     else:
